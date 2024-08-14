d217b3a1ff81e43a129a5484cdeea2e80b02cfa0
palakb9 <63990709+palakb9@users.noreply.github.com>
2024-08-14 01:04:30 +0000

    Saving the scraped data in current timestamp named files (txt file)

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import selenium
 import time
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-from selenium.webdriver.common import options
+from selenium.webdriver.common import by, options
+from datetime import datetime as dt
 
 def get_driver():
   # Set options to make browsing easier
@@ -18,17 +20,40 @@ def get_driver():
 
   driver.get("http://automated.pythonanywhere.com")
   return driver
-
+####
+#def clean_text(text):
+ # """Extract only the temperature from text"""
+ # output = float(text.split(": ")[1]) # spliting when there's a space
+  #return output
 def clean_text(text):
   """Extract only the temperature from text"""
-  output = float(text.split(": ")[1]) # spliting when there's a space
+  output = float(text.split(": ")[1]) 
   return output
+
+def write_file(text):
+  filename = f"{dt.now().strftime('%Y-%m-%d.%H-%M-%S')}.txt"
+  with open(filename, 'w') as file:
+    file.write(text)
 
 def main():
   driver = get_driver()
-  time.sleep(2) # Waiting for the page to load for 2 seconds
-  element = driver.find_element(by="xpath",value="/html/body/div[1]/div/h1[2]") # Finding the element using inspect
-  return element.text
+  #driver.find_element(by="id",value="id_username").send_keys("automated") 
+  #time.sleep(2)
+  #driver.find_element(by="id" , value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+  #time.sleep(3)
+  #driver.find_element(by="xpath",value="/html/body/nav/div/a").click()
+  #time.sleep(2)
+  #text =driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]").text
+  while True:
+    time.sleep(2)
+    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+    text = str(clean_text(element.text))
+    write_file(text)
+  
+  
+
+
+
 
 print(main())
-print(clean_text(main()))
+#print(clean_text(main()))
